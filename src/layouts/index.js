53e5bdc721a696b230c129f7c89a4a9f81d11492
Daniel Oshiro <sdobrado@gmail.com>
2018-07-11 12:55:54 -0300

    Remove gatsby default content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,26 +3,18 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Highlabs' },
+        { name: 'keywords', content: 'highlabs, frontend, freelancer, developer' },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
