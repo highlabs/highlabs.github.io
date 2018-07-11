f4472e4aff8d07bc9aa6171aab8879ba70fc0e3a
Daniel Oshiro <sdobrado@gmail.com>
2018-07-11 12:56:07 -0300

    typography init

---
+++ src/utils/typography.js
@@ -0,0 +1,11 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Open Sans', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Open Sans', 'serif'],
+});
+
+export default typography;
+
