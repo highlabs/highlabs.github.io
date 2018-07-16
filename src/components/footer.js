154fd777b26312f521c85f06fb5cfb3ad9a5beab
Daniel Oshiro <sdobrado@gmail.com>
2018-07-16 01:54:30 -0300

    simple button animation

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,9 @@ import './footer.scss'
 const Footer = () => (
   <footer className="footer">
       <h3>Interested in create something awesome?</h3>
-      <a className="button" href="mailto:daniel0oshiro@gmail.com">Email me</a>
+      <a className="button" href="mailto:daniel0oshiro@gmail.com">
+        <span>Email me</span>
+      </a>
   </footer>
 )
 
