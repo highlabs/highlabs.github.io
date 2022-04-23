993feca284144179b436cdf7059c6688e9cccf0b
Daniel Oshiro <daniel.oshiro@tutanota.com>
2022-04-23 17:01:03 +0100

    fix: delete site

--- path----8f41efd0303fdfd7d178.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Highlabs"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----8f41efd0303fdfd7d178.js.map
